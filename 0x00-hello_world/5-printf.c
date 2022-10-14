ca8ea5727d157c33dde66a8f1081dfaa074c4e09
JJAsore <asorejohn2030@gmai.com>
2022-10-13 18:11:45 -0700

    betyy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,31 @@
-#include <stdio.h>
-
+#include	<stdio.h>
 /**
+ * main	-	entry point
  *
- *  *  * main-entry point for the program.
- *
- *   *   *
- *
- *    *    * Return:0 if no errors, return non zero if errors
- *
- *     */
-
-int main(void)
-
+ * Return:	Always	0	(success)
+ */
+int	main(void)
 {
-
-			printf("with proper grammar, but the outcome is a piece of art,\n");
-
-						return (0);
-
+	printf("with	proper	grammar,	but	the	outcome		is	a	piece	of	art,\n");
+	return	(0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
