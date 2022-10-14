b490a488cc4110271a007b072daa8006836d53d5
JJAsore <asorejohn2030@gmai.com>
2022-10-13 17:55:40 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+
 /**
- * main-	Entry	point
  *
- * Return:	Always	0	(Success)
- */
-int	main(void)
+ *  *  * main-entry point for the program.
+ *
+ *   *   *
+ *
+ *    *    * Return:0 if no errors, return non zero if errors
+ *
+ *     */
+
+int main(void)
+
 {
-	printf("with	proper	grammar,	but	the	outcome	is	a	piece	of	art,\n");
-	return	(0);
+
+			printf("with proper grammar, but the outcome is a piece of art,\n");
+
+						return (0);
+
 }
