1f5569a24e082a494755c6c1ecdb67fac2b15a24
JJAsore <asorejohn2030@gmai.com>
2022-10-15 03:32:14 -0700

    uuu

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ n = rand() - RAND_MAX / 2;
 ldigit = n % 10;
 if	(ldigit	>	5)
 {
-printf("Last digit of	%d	is	%d	and	is	greater	than	5\n",	n,	ldigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, ldigit);
 }
 else	if	(ldigit	==	0)
 {
-printf("Last digit of %d is %d and is 0\n",   n,      ldigit);
+printf("Last digit of %d is %d and is 0\n",   n, ldigit);
 }
 else	if	(ldigit	<	6	&&	ldigit	!=	0)
 {
