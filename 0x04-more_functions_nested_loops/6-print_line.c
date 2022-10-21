2da85e684b1361dd619444f9c60712f7f34dacec
JJAsore <asorejohn2030@gmai.com>
2022-10-21 15:53:34 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_line-prints a straight line using '-'
+ *@n: the number of '-'
+ */
+void print_line(int n)
+{
+int len;
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+_putchar('-');
+}
+_putchar('\n');
+}
