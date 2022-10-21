110b9da8293b59320890b6ff35dae06e3eb1fd7b
JJAsore <asorejohn2030@gmai.com>
2022-10-21 12:51:04 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,9 @@
+#include <unistd.hh>
+#include "main.h"
+/**
+ * _putchar-writes the character c to stdout
+ * @c: the character to print
+ * Return: on success 1 else -1
+ */
+int _putchar(char c)
+return (writes (1, &c, 1));
