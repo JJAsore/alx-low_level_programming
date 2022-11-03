595c0c3e28e480756935d3940dbf61b212c4607b
JJAsore <asorejohn2030@gmai.com>
2022-11-02 22:42:16 -0700

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,8 +2,8 @@
 /**
  *wilcmp - compares two strings
  *@s1:string 1.
- *@s2:string 2. it can contains a * as a special character
- *Return:1 if are indentical , 0 if not.
+ *@s2:string 2. it can contains a* as a special character
+ *Return: 1 if are indentical , 0 if not.
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -12,6 +12,8 @@ return (0);
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
 if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
 return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 return (0);
 }
