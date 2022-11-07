048a15130f64829bab805be93ab1318facd5c9ad
JJAsore <asorejohn2030@gmai.com>
2022-11-07 01:00:07 -0800

    arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+/**
 *main-print name of the program
 *@argc: count arguments
 *@argv: arguments
 *Return:Always 0 (success)
 */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 (void) argc;
-printf("%d\n", argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -2,8 +2,7 @@
 /**
 *_putchar-writes the character c to stdout
 *@c: the character to print.
-*Return: on success 1.
-*on error, -1 is returned and error is set appropriately 
+*Return: on success 1.  *on error, -1 is returned and error is set appropriately
 */
 int _putchar(char c)
 {
