bcf234bfb00cbd0da1f628371c2622e035d04f15
JJAsore <asorejohn2030@gmai.com>
2022-11-15 04:22:46 -0800

    dog.h

---
+++ 0x0E-structures_typedef/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - print the character c to stdout
+ * @c: the character to print
+ * Return: 1 on success, on error return -1
+ * error is set appropriately
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
